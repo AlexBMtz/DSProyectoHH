e22dac84ab2dd3e69fb9f40cbe0f1ab26612683c
AlexBMtz <barroetaalejandro160401@gmail.com>
2021-08-25 16:51:52 -0500

    Creación de la arquitectura básica del proyecto

---
+++ DSProyectoHH.Web/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using DSProyectoHH.Web.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSProyectoHH.Web.Data
+{
+    public class DataContext : IdentityDbContext<User>
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
