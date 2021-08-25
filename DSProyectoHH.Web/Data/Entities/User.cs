ce501451ec583a996e68cc6fd7d09c84a32a7d36
AlexBMtz <barroetaalejandro160401@gmail.com>
2021-08-25 17:01:53 -0500

    Adición de paquetes NuGets para la creación de la BD

--- DSProyectoHH.Web/Data/Entities/User.cs
+++ DSProyectoHH.Web/Data/Entities/User.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,30 @@ namespace DSProyectoHH.Web.Data.Entities
 {
     public class User:IdentityUser
     {
+        [Required(ErrorMessage = "{0} es obligatorio.")]
 
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+
+        [Display(Name = "Nombre")]
+
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "{0} es obligatorio.")]
+
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+
+        [Display(Name = "Apellidos")]
+
+        public string LastName { get; set; }
+
+        [MaxLength(10, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+
+        [Display(Name = "Número de Celular")]
+
+        public override string PhoneNumber { get; set; }
+
+        [Display(Name = "Nombre")]
+
+        public string FullName => $"{LastName} {FirstName}";
     }
 }
--- DSProyectoHH.Web/Startup.cs
+++ DSProyectoHH.Web/Startup.cs
@@ -1,6 +1,10 @@
+using DSProyectoHH.Web.Data;
+using DSProyectoHH.Web.Data.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +28,22 @@ namespace DSProyectoHH.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddIdentity<User, IdentityRole>(cfg =>
+            {
+                cfg.User.RequireUniqueEmail = true;
+                cfg.Password.RequireDigit = false;
+                cfg.Password.RequiredUniqueChars = 0;
+                cfg.Password.RequireLowercase = false;
+                cfg.Password.RequireNonAlphanumeric = false;
+                cfg.Password.RequireUppercase = false;
+                cfg.Password.RequiredLength = 6; 
+            })
+
+           .AddEntityFrameworkStores<DataContext>();
+            services.AddDbContext<DataContext>(cfg =>
+            {
+                cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConection"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
