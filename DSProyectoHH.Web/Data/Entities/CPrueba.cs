18ad69c0548599359e8f52874ab5228ebed5e56a
Jesus PM <jesus.puon@gmail.com>
2021-08-27 15:05:59 -0500

    Prueba de Puón

---
+++ DSProyectoHH.Web/Data/Entities/CPrueba.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSProyectoHH.Web.Data.Entities
+{
+    public class CPrueba
+    {
+        //Prueba Eduardo
+    }
+}
