16cf7b2e12bca1e869568671d1f4e8f1daa38dbd
Edwin Lozada Ramos <edwin20.elr@outlook.com>
2021-09-01 23:49:40 -0500

    Creación de la Entidad Course

---
+++ DSProyectoHH.Web/Data/Entities/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSProyectoHH.Web.Data.Entities
+{
+    public class Course
+    {
+        private int id_course;
+        private string description;
+        private string start_date;
+
+        public int Id_Course { get { return id_course; } set { id_course = value; } }
+        public string Description { get { return description; } set { description = value; } }
+        public string Start_Date { get { return start_date; } set { start_date = value; } }
+    }
+}
