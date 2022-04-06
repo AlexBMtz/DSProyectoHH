baa163891b7a69357fa66174acf32c84491cb8dd
Edwin Lozada R <edwin20.elr@gmail.com>
2022-04-05 22:08:39 -0500

    Conexión SQLServer establecida exitosamente.

--- DSProyectoHH.Web/Startup.cs
+++ DSProyectoHH.Web/Startup.cs
@@ -71,12 +71,21 @@ namespace DSProyectoHH.Web
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
 
             app.UseStaticFiles();
             app.UseAuthentication();
 
             app.UseRouting();
+
+            app.Use(async (context, next) =>
+            {
+                await next();
+                if (context.Response.StatusCode == 404)
+                {
+                    context.Request.Path = "/Home";
+                    await next();
+                }
+            });
 
             app.UseAuthorization();
 
